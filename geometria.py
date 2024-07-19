17e550d582e2eca33ba9a53beeb50bbb1ae0fd24
lina-rms <alinerl.eu30@gmail.com>
2024-07-19 00:36:45 -0400

    Adicionando erro ao cod para reproduzir cenário 2 (erro na build)

--- geometria.py
+++ geometria.py
@@ -3,8 +3,7 @@ import math
 def area_circulo(raio):
     if raio < 0:
         raise ValueError("O raio não pode ser negativo.")
-   ## return math.pi * raio ** 2
-    return math.pi * raio ** 4 ## inserindo erro para reproduzir cenário 3 (erro nos testes buld instavel)
+    return math.pi * raio ** 2
 
 def area_retangulo(largura, altura):
     if largura < 0 or altura < 0:
